334384bfc74f1b0bf9c4c716863e4231c97b3d85
hiroko01 <hirokosakamoto31@gmail.com>
2023-12-02 20:04:29 +0900

    ナンバリング

--- html-css/grid-layout/app.js
+++ html-css/grid-layout/app.js
@@ -1,6 +1,62 @@
-console.log("Hello World!");
 
-//1. display:flexを用いたグリッドレイアウト
-// 1. 横4 × 縦4
-// 2. 横2 × 縦2
-// 3. 横6 × 縦1
+// document.addEventListener("DOMContentLoaded", function () {
+//     let list = document.getElementById("js-number");
+//     let listItems = list.getElementsByTagName("li");
+
+//     for (let i = 0; i < listItems.length; i++) {
+
+//         // 2桁の数字にフォーマット
+//         let twoDigitNumber = ("0" + (i + 1)).slice(-2);
+//         listItems[i].textContent = twoDigitNumber;
+
+
+//     }
+// });
+
+// document.addEventListener("DOMContentLoaded", function () {
+//     let list = document.getElementById("js-number-2");
+//     let listItems = list.getElementsByTagName("li");
+
+//     for (let i = 0; i < listItems.length; i++) {
+
+//         // 2桁の数字にフォーマット
+//         let twoDigitNumber = ("0" + (i + 1)).slice(-2);
+//         listItems[i].textContent = twoDigitNumber;
+
+
+//     }
+// });
+
+// document.addEventListener("DOMContentLoaded", function () {
+//     let list = document.getElementById("js-number-3");
+//     let listItems = list.getElementsByTagName("li");
+
+//     for (let i = 0; i < listItems.length; i++) {
+
+//         // 2桁の数字にフォーマット
+//         let twoDigitNumber = ("0" + (i + 1)).slice(-2);
+//         listItems[i].textContent = twoDigitNumber;
+
+
+//     }
+// });
+
+
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    numberListItems("js-number-1");
+    numberListItems("js-number-2");
+    numberListItems("js-number-3");
+});
+
+function numberListItems(listId) {
+    let list = document.getElementById(listId);
+    let listItems = list.getElementsByTagName("li");
+
+    for (let i = 0; i < listItems.length; i++) {
+        let twoDigitNumber = ("0" + (i + 1)).slice(-2);
+        listItems[i].textContent = twoDigitNumber;
+    }
+}
