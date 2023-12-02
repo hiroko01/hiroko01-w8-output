4e2b95a51a56621939c9263e21a600acd7bda8ac
hiroko01 <hirokosakamoto31@gmail.com>
2023-12-02 11:52:14 +0900

    grid-layout できた　グレー×ホワイト　次はフォントやろう

--- html-css/grid-layout/app.js
+++ html-css/grid-layout/app.js
@@ -1,1 +1,6 @@
 console.log("Hello World!");
+
+//1. display:flexを用いたグリッドレイアウト
+// 1. 横4 × 縦4
+// 2. 横2 × 縦2
+// 3. 横6 × 縦1
--- html-css/login-form/app.js
+++ html-css/login-form/app.js
@@ -1,1 +1,6 @@
 console.log("Hello World!");
+
+// 2. ログインフォーム
+//     1. email, passwordの入力欄がある
+//     2. ボタンがある
+//     3. ボタンの下にSign upページへのリンクがある
