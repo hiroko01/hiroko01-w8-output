57d4507a4a1dfa8e5c85e6b9b616767e0ec98b21
hiroko01 <hirokosakamoto31@gmail.com>
2023-12-03 09:07:44 +0900

    no message

--- database/app.js
+++ database/app.js
@@ -1,62 +1,6 @@
+console.log("Hello World!");
 
-// document.addEventListener("DOMContentLoaded", function () {
-//     let list = document.getElementById("js-number");
-//     let listItems = list.getElementsByTagName("li");
-
-//     for (let i = 0; i < listItems.length; i++) {
-
-//         // 2桁の数字にフォーマット
-//         let twoDigitNumber = ("0" + (i + 1)).slice(-2);
-//         listItems[i].textContent = twoDigitNumber;
-
-
-//     }
-// });
-
-// document.addEventListener("DOMContentLoaded", function () {
-//     let list = document.getElementById("js-number-2");
-//     let listItems = list.getElementsByTagName("li");
-
-//     for (let i = 0; i < listItems.length; i++) {
-
-//         // 2桁の数字にフォーマット
-//         let twoDigitNumber = ("0" + (i + 1)).slice(-2);
-//         listItems[i].textContent = twoDigitNumber;
-
-
-//     }
-// });
-
-// document.addEventListener("DOMContentLoaded", function () {
-//     let list = document.getElementById("js-number-3");
-//     let listItems = list.getElementsByTagName("li");
-
-//     for (let i = 0; i < listItems.length; i++) {
-
-//         // 2桁の数字にフォーマット
-//         let twoDigitNumber = ("0" + (i + 1)).slice(-2);
-//         listItems[i].textContent = twoDigitNumber;
-
-
-//     }
-// });
-
-
-
-
-
-document.addEventListener("DOMContentLoaded", function () {
-    numberListItems("js-number-1");
-    numberListItems("js-number-2");
-    numberListItems("js-number-3");
-});
-
-function numberListItems(listId) {
-    let list = document.getElementById(listId);
-    let listItems = list.getElementsByTagName("li");
-
-    for (let i = 0; i < listItems.length; i++) {
-        let twoDigitNumber = ("0" + (i + 1)).slice(-2);
-        listItems[i].textContent = twoDigitNumber;
-    }
-}
+// 2. ログインフォーム
+//     1. email, passwordの入力欄がある
+//     2. ボタンがある
+//     3. ボタンの下にSign upページへのリンクがある
