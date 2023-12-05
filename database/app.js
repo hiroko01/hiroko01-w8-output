99bcfc007e5daa6a6f27111142527a76192e9fe8
hiroko01 <hirokosakamoto31@gmail.com>
2023-12-06 00:05:08 +0900

    emailは記録されるようになった

--- database/app.js
+++ database/app.js
@@ -31,21 +31,42 @@ const $email = document.getElementById("js-email");
 localStorage.setItem("email","value");
 
 const localStorageVal = localStorage.getItem("email");
-console.log(localStorageVal);
-
-
-document.getElementById("js-password");
-const $password = document.getElementById("js-password");
-
-localStorage.setItem("password","value")
-
-const localStorageVal_pass = localStorage.getItem("password");
-console.log(localStorageVal_pass);
+// console.log(localStorageVal);
 
 
 
 
+// document.getElementById("js-password");
+// const $password = document.getElementById("js-password");
 
+// localStorage.setItem("password","value")
+
+// const localStorageVal_pass = localStorage.getItem("password");
+// console.log(localStorageVal_pass);
+
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    // 保存された email と password を取得
+    const savedEmail = localStorage.getItem('savedEmail');
+    const savedPassword = localStorage.getItem('savedPassword');
+  
+    // フォームに保存された email と password を自動入力
+    if (savedEmail) document.getElementById('email').value = savedEmail;
+    if (savedPassword) document.getElementById('password').value = savedPassword;
+  });
+  
+  function signIn() {
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+  
+    // ログイン処理の代わりに、入力された email と password を保存
+    localStorage.setItem('savedEmail', email);
+    localStorage.setItem('savedPassword', password);
+  
+    alert('Logged In!');
+  }
 
 
 
